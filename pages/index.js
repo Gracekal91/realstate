c1ca43c1c401abe34a1101245b5f7af0d2fcfee0
damien <damienmende30@gmail.com>
2021-12-31 09:24:47 +0200

    next js setup and dependencies installed

---
+++ pages/index.js
@@ -0,0 +1,50 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { Flex, Box, Text, Button } from '@chakra-ui/react';
+
+const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => {
+  return (
+    <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
+      <Image src={imageUrl} width={500} height={300} alt="banner" />
+      <Box p="5">
+        <Text color="gray.500" fontSize="sm" fontWeight="medium">{purpose}</Text>
+        <Text color="gray.500" fontSize="3xl" fontWeight="bold">{title1} <br />{title2}</Text>
+        <Text fontSize="lg" paddingTop="3" paddingBottom="3" color="gray.700">{desc1} <br />{desc2}</Text>
+        <Button fontsize="xl" >
+          <a href={linkName}>{buttonText}</a>
+        </Button>
+      </Box>
+    </Flex>
+  )
+
+}
+
+export default function Home() {
+  return (
+    <div>
+      <Banner
+        purpose="RENT A HOME"
+        title1="Rental Homes for"
+        title2="Everyone"
+        buttonText="Rent home"
+        desc1="Explore apartments, Villas, Homes"
+        desc2="and more"
+        linkName="/search?purpose=for-rent"
+        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+
+      />
+
+      <Banner
+        purpose="BUY A HOME"
+        title1=" find, buy & Own your dream Homes for"
+        title2="Dream Home"
+        desc1="Explore apartments, Villas, Homes"
+        desc2="explore buying"
+        linkName="/search?purpose=for-sale"
+        buttonText="Buy home"
+        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+
+      />
+    </div>
+  )
+}
